9c1c38516149e9f3cc1bdba5370da4dc6cdf1a8d
Yotsuba000 <i.lohas.apple01@gmail.com>
2016-11-05 20:40:36 +0900

    up

---
+++ MainInterface/js/HeaderChange.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    	$(document).ready(function () {
+		var ImageMaxCount = 9;			//画像枚数
+		var ImageNumber = Math.floor(Math.random() * ImageMaxCount);
+		var Uri = "https://dl.dropbox.com/u/70568694/MainInterface/fab_title_bg_" + ImageNumber + ".png";
+		
+		if(ImageNumber != 0) {
+			$("#header").bind("click", function () {
+				location.href = "http://yotsubadiary3.blog.fc2.com/";
+			});
+			$("#header").bind("hover", function () {
+				$("#header").css("cursor", "pointer");
+			});
+			$("#title > h1").hide();
+		}
+
+		$("#header").css("background-image", "url(" + Uri + ")" );
+	});
+})();
---
+++ MapCutResistance/js/LoadText.js
@@ -0,0 +1,68 @@
+﻿$(function() {
+	$.ajax({
+		type: 'GET',
+		url: 'Map_PlayerCutRegstance.txt',
+		dataType: "text",
+		success: function(data) {
+			$('#result_1').append(CreateData(data));
+		},
+		error:function() {
+			alert('ファイルの読み込みに失敗しました。');
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url: 'expansion_Map_PlayerCutRegstance.txt',
+		dataType: "text",
+		success: function(data) {
+			$('#result_2').append(CreateData(data));
+		},
+		error:function() {
+			alert('ファイルの読み込みに失敗しました。');
+		}
+	});	
+	
+	function CreateData(tData){
+		var $table = $('<table />');
+		var TextObj = tData;
+		var Sentence = TextObj.split('\r\n');
+    	for(var i = 0; i < Sentence.length; i++)
+   		{
+   			var $tr = $('<tr />');
+   			var Word = Sentence[i].split('\t');
+   			for(var j = 0; j < Word.length; j++)
+   			{
+   				if(j == (Word.length -1))
+   				{
+   					var $td = $('<th />').append(Word[j]);
+   				}
+   				else
+   				{
+   					var $td = $('<td />').append(Word[j]);
+   				}
+   				if(i == 0)
+   				{ 
+   					$td.css({
+   						"font-weight": "bold",
+   						"background-color": "#eeff00"
+   					});
+   				}
+   				if(i != 0 && j != (Word.length -1))
+   				{
+   					if(Word[j] != "0")
+   					{
+	   					$tr.css({
+	   						"background-color": "#c0c0ff"
+	   					});	
+   					}
+   				}
+   				$tr.append($td);
+    		}
+    		$table.append($tr);
+    	}
+    	$table.addClass('table-01');
+    	return $table;
+	}
+	
+});
